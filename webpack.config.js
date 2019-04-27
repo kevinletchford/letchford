2f04a791569e5ddbc8c4ae922f33291ae356206d
kevinletchford <615803+kevinletchford@users.noreply.github.com>
2019-04-27 21:21:24 +0100

    changed folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ const htmlPlugins = generateHtmlPlugins("./src/pages/");
 module.exports = {
   entry: "./src/scripts/scripts.js",
   output: {
-    path: path.resolve(__dirname, "gh-pages"), // Output folder
+    path: path.resolve(__dirname, "docs"), // Output folder
     filename: "js/app.js" // JS output path
   },
 
