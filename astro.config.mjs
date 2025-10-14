fadaf40ecfc42f25b73c3134e6b37257e1a703de
Kevin Letchford <kextype@gmail.com>
2025-10-14 23:12:01 +0100

    added trailing / to all urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+    trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss(),         
               glsl()],
